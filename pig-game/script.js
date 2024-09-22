e36417873ecd3b21a457fbf7a14acb2965e3fe87
abdosamiir <barakatabdo82@gmail.com>
2024-09-22 19:52:21 +0300

    refactor some code

--- pig-game/script.js
+++ pig-game/script.js
@@ -13,14 +13,22 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let currentScore, activePlayer, playing, scores;
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  scores = [0, 0];
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+};
 
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-const scores = [0, 0];
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -55,7 +63,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
 
@@ -72,15 +80,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  currentScore = 0;
-  playing = true;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  diceEl.classList.add('hidden');
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
-
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-});
+btnNew.addEventListener('click', init);
