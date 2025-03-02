a67ae14cf01d8ec1846a893e2397196b732e9313
abdosamiir <barakatabdo82@gmail.com>
2025-03-02 12:24:11 +0200

    simplify player assignment using array destructuring

--- operator-challengs/script.js
+++ operator-challengs/script.js
@@ -41,8 +41,9 @@ const game = {
   },
 };
 
-const players1 = game.players[0];
-const players2 = game.players[1];
+// const players1 = game.players[0];
+// const players2 = game.players[1];
+const [players1, players2] = game.players;
 console.log(players1, players2);
 
 const [gk, ...fieldPlayers] = players1;
