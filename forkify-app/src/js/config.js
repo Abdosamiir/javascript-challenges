0a7468bf3d3ac559d1ea884d594d079ebdc3e4dd
abdosamiir <barakatabdo82@gmail.com>
2025-05-14 04:38:26 +0300

    feat: Initialize project structure and implement core functionality
    - Added configuration file for API settings.
    - Developed main controller to fetch and display recipe data from the API.
    - Implemented loading spinner for better user experience during data fetching.
    - Created base styles and layout for the application using SASS.
    - Designed reusable button components with hover effects.
    - Established header and navigation components for improved UI.
    - Developed recipe display with detailed information and ingredients list.
    - Implemented search results layout and pagination for user-friendly navigation.
    - Created upload modal for adding new recipes with appropriate styling.

---
+++ forkify-app/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5d201458-4bff-4417-9f1a-4b776e7580b7';
+export const MODAL_CLOSE_SEC = 2.5;
