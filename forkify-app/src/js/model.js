313b8b862f236de1c1efb19b6450dd410a602500
abdosamiir <barakatabdo82@gmail.com>
2025-05-22 15:21:20 +0300

    feat: Implement servings update functionality in recipe view

--- forkify-app/src/js/model.js
+++ forkify-app/src/js/model.js
@@ -53,7 +53,7 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-loadSearchResults('pizza');
+// loadSearchResults('pizza');
 
 export const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
@@ -61,4 +61,11 @@ export const getSearchResultsPage = function (page = state.search.page) {
   const end = page * state.search.resultsPerPage; // 10
 
   return state.search.results.slice(start, end);
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+  });
+  state.recipe.servings = newServings;
 };
