dd55869b1a13938b44892317863ae2a8a5bf2d2a
abdosamiir <barakatabdo82@gmail.com>
2025-10-08 21:51:32 +0300

    feat: Add sorting functionality to display movements; implement loan feature and calculate total account movements

--- bankist-Array/script.js
+++ bankist-Array/script.js
@@ -65,10 +65,12 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = ''; // Clear existing movements
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
       <div class="movements__row">
@@ -268,3 +270,123 @@ console.log(latestLargeMovIndex);
 console.log(
   `your latest large mov was ${movements.length - latestLargeMovIndex} ago`
 );
+
+btnLoan.addEventListener('click', e => {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && curAcc.movements.some(mov => mov >= amount * 0.1)) {
+    // add movement
+    curAcc.movements.push(amount);
+    // update UI
+    updateUI(curAcc);
+    inputLoanAmount.value = '';
+  }
+});
+
+const deposit = mov => mov > 0;
+
+// const accountMov = accounts.map(acc => acc.movements);
+// console.log(accountMov.flat());
+
+const accMovements = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((sum, mov) => sum + mov, 0);
+console.log(accMovements);
+
+// Coding Challenge #4
+
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+*/
+
+// const breeds = [
+//   {
+//     breed: 'German Shepherd',
+//     averageWeight: 32,
+//     activities: ['fetch', 'swimming'],
+//   },
+//   {
+//     breed: 'Dalmatian',
+//     averageWeight: 24,
+//     activities: ['running', 'fetch', 'agility'],
+//   },
+//   {
+//     breed: 'Labrador',
+//     averageWeight: 28,
+//     activities: ['swimming', 'fetch'],
+//   },
+//   {
+//     breed: 'Beagle',
+//     averageWeight: 12,
+//     activities: ['digging', 'fetch'],
+//   },
+//   {
+//     breed: 'Husky',
+//     averageWeight: 26,
+//     activities: ['running', 'agility', 'swimming'],
+//   },
+//   {
+//     breed: 'Bulldog',
+//     averageWeight: 36,
+//     activities: ['sleeping'],
+//   },
+//   {
+//     breed: 'Poodle',
+//     averageWeight: 18,
+//     activities: ['agility', 'fetch'],
+//   },
+// ];
+
+// const huskyWeight = breeds.find(breed => breed.breed === 'Husky').averageWeight;
+// console.log(huskyWeight);
+
+// const dogBothActivities = breeds.find(
+//   breed =>
+//     breed.activities.includes('running') && breed.activities.includes('fetch')
+// ).breed;
+
+// console.log(dogBothActivities);
+
+// const allActivities = breeds.flatMap(breed => breed.activities);
+// console.log(allActivities);
+
+// const uniqueActivities = [...new Set(allActivities)];
+// console.log(uniqueActivities);
+
+// const swimmingAdjacent = breeds
+//   .filter(breed => breed.activities.includes('swimming'))
+//   .flatMap(breed => breed.activities)
+//   .filter(activity => activity !== 'swimming');
+
+// console.log([...new Set(swimmingAdjacent)]);
+
+// console.log(breeds.every(breed => breed.averageWeight > 10));
+// console.log(breeds.some(breed => breed.activities.length >= 3));
+
+// const fetchWeights = breeds
+//   .filter(breed => breed.activities.includes('fetch'))
+//   .map(breed => breed.averageWeight);
+// const heaviestFetchBreed = Math.max(...fetchWeights);
+// console.log(fetchWeights);
+// console.log(heaviestFetchBreed);
+
+let sorted = false;
+btnSort.addEventListener('click', e => {
+  e.preventDefault();
+  displayMovements(curAcc.movements, !sorted);
+  sorted = !sorted;
+});
+
